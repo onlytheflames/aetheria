bf84b79f9f3df3e16f90e16005ca843c26da0d27
Sushitr4sh <mariodaruranto68@gmail.com>
2025-08-13 07:25:02 +0800

    Migrate to Gemini 2.0

--- app/api/journals/[userId]/details/[journalId]/route.ts
+++ app/api/journals/[userId]/details/[journalId]/route.ts
@@ -3,7 +3,7 @@ import Journal from "@/models/journal";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export async function GET(
   req: Request,
